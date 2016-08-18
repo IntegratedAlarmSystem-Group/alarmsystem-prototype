a7e72b902e73133c7a7213ed08a1bfc0fa117aae
Alessandro Caproni <acaproni@eso.org>
2016-08-18 11:49:56 +0200

    Removes mpID That was already instantiated in the constructor.
    Adds setter and getter for the value of the monitor point.

--- BasicTypes/src/org/eso/ias/prototype/input/MonitorPoint.scala
+++ BasicTypes/src/org/eso/ias/prototype/input/MonitorPoint.scala
@@ -11,16 +11,34 @@ object OperationalMode extends Enumeration {
 /**
  * A monitor point holds the value of a monitor point.
  * It is parametrized because a monitor point can be a double an integer, an
- * array of integers and many other types.  
+ * array of integers and many other types.
+ * 
+ * <code>MonitorPoint</code> is intended for internal use only:
+ * object of this type shall not be instantiated directly (this class has, in fact be made abstract)
+ * <code>org.eso.ias.prototype.input.typedmp</code> provides a set of types
+ * for the monitor points.    
  */
-class MonitorPoint[A](
-    id: String,
-    runningMode: OperationalMode.Mode = OperationalMode.Running) {
+abstract class MonitorPoint[A](
+    val id: String,
+    val runningMode: OperationalMode.Mode = OperationalMode.Running) {
   
-  // The value of the monitor point
-  // Uninitialized at build time
-  var actualValue: Option[A] = None
+  /**
+   *  The value of the monitor point
+   *  Uninitialized at build time
+   */
+  private var actualValue: Option[A] = None
   
-  // The unique ID of this monitor point
-  val mpID: String = id
+  /**
+   * Getter
+   */
+  protected def setValue(v: A): Unit = {
+    actualValue = Option(v)
+  }
+  
+  /**
+   * Setter
+   */
+  def getValue: Option[A] = {
+    actualValue
+  }
 }
