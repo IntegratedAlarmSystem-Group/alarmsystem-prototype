72d1703cd206eb251d0e9cdc554c0170dcaeca82
Alessandro Caproni <acaproni@eso.org>
2016-08-18 11:52:13 +0200

    typedmp package contains the monitor points for specific types like integer, long and so on. LongMP is a monitor point of Long type.
    MonitorPoint should be improved with a factory method for instantiating
    a monitor point of the right type.

---
+++ BasicTypes/src/org/eso/ias/prototype/input/typedmp/LongMP.scala
@@ -0,0 +1,14 @@
+package org.eso.ias.prototype.input.typedmp
+
+import org.eso.ias.prototype.input.MonitorPoint
+import org.eso.ias.prototype.input.OperationalMode
+
+/**
+ * <code>LongMP</code> implements a MonitorPoint for the type Long.
+ */
+case class LongMP(
+    mpID: String,
+    mode: OperationalMode.Mode = OperationalMode.Running) 
+    extends MonitorPoint[Long](mpID,mode) {
+  
+}
---
+++ BasicTypes/src/org/eso/ias/prototype/input/typedmp/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * This package provides support for typed monitor points.
+ * 
+ * Being this a prototype, this package will not provide all the possible
+ * monitor point types:
+ * <UL>
+ * 	<LI>Integer and Long
+ *  <LI>Float and Double
+ *  <LI>Boolean
+ *  <LI>Char and String
+ *  <LI>(32 and 64) Bit mask
+ *  <LI>Tuples 
+ * </UL>
+ * 
+ * However, the types provided here should allow to test 
+ * the prototype.
+ * 
+ * @author acaproni
+ *
+ */
+package org.eso.ias.prototype.input.typedmp;
