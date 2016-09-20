55517a910db6526430dcc78f9a6dd1696399a15e
Alessandro Caproni <acaproni@eso.org>
2016-09-20 16:13:13 +0200

    Renames running mode to operational.

--- BasicTypes/src/org/eso/ias/prototype/input/MonitorPoint.scala
+++ BasicTypes/src/org/eso/ias/prototype/input/MonitorPoint.scala
@@ -5,22 +5,22 @@ package org.eso.ias.prototype.input
  */
 object OperationalMode extends Enumeration {
   type Mode = Value
-  val StartUp, ShutDown, Maintenance, Running = Value
+  val StartUp, ShutDown, Maintenance, Operational = Value
 }
 
 /**
  * A monitor point holds the value of a monitor point.
  * It is parametrized because a monitor point can be a double an integer, an
- * array of integers and many other types.
+ * array of integers and many other types. (Should we look for a better name?)
  * 
  * <code>MonitorPoint</code> is intended for internal use only:
- * object of this type shall not be instantiated directly (this class has, in fact be made abstract)
+ * objects of this type shall not be instantiated directly (this class has, in fact abstract)
  * <code>org.eso.ias.prototype.input.typedmp</code> provides a set of types
  * for the monitor points.    
  */
 abstract class MonitorPoint[A](
     val id: String,
-    val runningMode: OperationalMode.Mode = OperationalMode.Running) {
+    val runningMode: OperationalMode.Mode = OperationalMode.Operational) {
   
     /**
      * The value of the monitoring point is associated 
