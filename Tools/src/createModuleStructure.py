7e98fcadb06041c39f3adb5ff67ce02437730c6a
Alessandro Caproni <acaproni@eso.org>
2016-09-22 16:39:59 +0200

    Deine a class with static methods instead globad functions

--- Tools/src/createModuleStructure.py
+++ Tools/src/createModuleStructure.py
@@ -5,63 +5,78 @@ Created on Aug 18, 2016
 @author: acaproni
 '''
 
-from os.path import exists, sep, join
+from os.path import exists, join
 from os import makedirs
 import argparse
 from shutil import rmtree, copyfile
 from iasFindFile import iasFindFile
 
-def writeLicenseFile(folder):
+class IASModuleSupport(object):
     """
-    Create a file with the license in the passed folder
+    A class providing useful method for dealing with IAS
+    modules including IAS_ROOT)
+    
+    @raise IOError: if the folder is not writable or the license file is not found
+    @raise ValueError: if the passed folder name is None or Empty
     """
-    licenseFile = iasFindFile("LPGPv3License.txt","config")
-    copyfile(licenseFile,join(folder,"LGPLv3.txt"))
-
-def createModule(name):
-    """
-    Create a IAS empty module.
-    
-    The text of the LGPL license will be copied in the root of the module.
-    
-    @param name: The name (full path) of the module to create.
-    
-    @return: 0 in case of success; -1 otherwise
-    """
-    
-    # Read the list of folders to create from the template
-    listOfFoldersFileName = iasFindFile("FoldersOfAModule.template","config")
-    with open(listOfFoldersFileName) as f:
-        folders = f.readlines()
-    # Check if the module 
-    if not exists(name):
-        print "Creating module",name
-        makedirs(name)
-        writeLicenseFile(name)
-        for folder in folders:
-            # Remove comments i.e. #...
-            parts = folder.partition('#')
-            folderName=parts[0].strip()
-            if folderName:
-                makedirs(join(name,folderName))
+    @staticmethod
+    def writeLicenseFile(rootOfModule):
+        """
+        Create a file with the license in the passed rootOfModule
         
-        return 0
-    else:
-        print
-        print name,"already exists!!!"
-        print "Use -e|--erase to remove before creating the module."
-        print
-        return -1
+        @param The root folder of the module
+        """
+        if not rootOfModule:
+            raise ValueError("The root of the module can't be None nor empty")
+        licenseFile = iasFindFile("LPGPv3License.txt","config")
+        copyfile(licenseFile,join(rootOfModule,"LGPLv3.txt"))
 
-def removeExistingModule(name):
-    '''
-    Remove an existing module
-    
-    @param name: The full path name of the module to remove
-    '''
-    print "Removing moldule",name
-    if exists(name):
-        rmtree(name) 
+    @staticmethod
+    def createModule(name):
+        """
+        Create a IAS empty module.
+        
+        The text of the LGPL license will be copied in the root of the module.
+        
+        @param name: The name (full path) of the module to create.
+        
+        @return: 0 in case of success; -1 otherwise
+        @see: self.writeLicenseFile
+        """
+        if not name:
+            raise ValueError("The name of the module can't be None nor empty")
+        # Read the list of folders to create from the template
+        listOfFoldersFileName = iasFindFile("FoldersOfAModule.template","config")
+        with open(listOfFoldersFileName) as f:
+            folders = f.readlines()
+        # Check if the module 
+        if not exists(name):
+            print "Creating module",name
+            makedirs(name)
+            IASModuleSupport.writeLicenseFile(name)
+            for folder in folders:
+                # Remove comments i.e. #...
+                parts = folder.partition('#')
+                folderName=parts[0].strip()
+                if folderName:
+                    makedirs(join(name,folderName))
+            
+            return 0
+        else:
+            raise IOError(name+"already exists!!!")
+
+    @staticmethod
+    def removeExistingModule(name):
+        '''
+        Remove an existing module
+        
+        @param name: The full path name of the module to remove
+        '''
+        if not name:
+            raise ValueError("The name of the module can't be None nor empty")
+        print "Removing moldule",name
+        if exists(name):
+            rmtree(name) 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Creates a module for the Integrated Alarm System.')
@@ -75,6 +90,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     if args.erase:
-        removeExistingModule(args.moduleName)
-    createModule(args.moduleName)
+        try:
+            IASModuleSupport.removeExistingModule(args.moduleName)
+        except Exception, e:
+            print "Error deleting the module: ",str(e)
+            exit(-1)
+    try:
+        IASModuleSupport.createModule(args.moduleName)
+    except Exception, e:
+        print "Error creating the module: ",str(e)
+        exit(-1)
     
