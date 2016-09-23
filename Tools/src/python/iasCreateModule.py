edba97be0a15c00def45a1da1d4e715c88ec71b6
Alessandro Caproni <acaproni@eso.org>
2016-09-23 14:00:02 +0200

    Gets command line parameters and delegate execution to classes from IASTools module

--- Tools/src/python/iasCreateModule.py
+++ Tools/src/python/iasCreateModule.py
@@ -5,78 +5,10 @@ Created on Aug 18, 2016
 @author: acaproni
 '''
 
-from os.path import exists, join
-from os import makedirs
 import argparse
-from shutil import rmtree, copyfile
-from iasFindFile import iasFindFile
 
-class IASModuleSupport(object):
-    """
-    A class providing useful method for dealing with IAS
-    modules including IAS_ROOT)
-    
-    @raise IOError: if the folder is not writable or the license file is not found
-    @raise ValueError: if the passed folder name is None or Empty
-    """
-    @staticmethod
-    def writeLicenseFile(rootOfModule):
-        """
-        Create a file with the license in the passed rootOfModule
-        
-        @param The root folder of the module
-        """
-        if not rootOfModule:
-            raise ValueError("The root of the module can't be None nor empty")
-        licenseFile = iasFindFile("LPGPv3License.txt","config")
-        copyfile(licenseFile,join(rootOfModule,"LGPLv3.txt"))
+from IASTools.ModuleSupport import ModuleSupport
 
-    @staticmethod
-    def createModule(name):
-        """
-        Create a IAS empty module.
-        
-        The text of the LGPL license will be copied in the root of the module.
-        
-        @param name: The name (full path) of the module to create.
-        
-        @return: 0 in case of success; -1 otherwise
-        @see: self.writeLicenseFile
-        """
-        if not name:
-            raise ValueError("The name of the module can't be None nor empty")
-        # Read the list of folders to create from the template
-        listOfFoldersFileName = iasFindFile("FoldersOfAModule.template","config")
-        with open(listOfFoldersFileName) as f:
-            folders = f.readlines()
-        # Check if the module 
-        if not exists(name):
-            print "Creating module",name
-            makedirs(name)
-            IASModuleSupport.writeLicenseFile(name)
-            for folder in folders:
-                # Remove comments i.e. #...
-                parts = folder.partition('#')
-                folderName=parts[0].strip()
-                if folderName:
-                    makedirs(join(name,folderName))
-            
-            return 0
-        else:
-            raise IOError(name+"already exists!!!")
-
-    @staticmethod
-    def removeExistingModule(name):
-        '''
-        Remove an existing module
-        
-        @param name: The full path name of the module to remove
-        '''
-        if not name:
-            raise ValueError("The name of the module can't be None nor empty")
-        print "Removing moldule",name
-        if exists(name):
-            rmtree(name) 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Creates a module for the Integrated Alarm System.')
@@ -91,12 +23,12 @@ if __name__ == '__main__':
     
     if args.erase:
         try:
-            IASModuleSupport.removeExistingModule(args.moduleName)
+            ModuleSupport.removeExistingModule(args.moduleName)
         except Exception, e:
             print "Error deleting the module: ",str(e)
             exit(-1)
     try:
-        IASModuleSupport.createModule(args.moduleName)
+        ModuleSupport.createModule(args.moduleName)
     except Exception, e:
         print "Error creating the module: ",str(e)
         exit(-1)
--- Tools/src/python/iasFindFile.py
+++ Tools/src/python/iasFindFile.py
@@ -5,94 +5,14 @@ Created on Sep 21, 2016
 @author: acaproni
 '''
 import argparse
-import os
-
-# Possible types of files recognized by this tool
-#
-# iasFileType matches with the folder names of a module
-# to ensure a valid value is given here
-iasFileType = ("lib", "bin","config","src")
-
-def recursivelyLookForFile(folder,name):
-    """
-    Search recursively for a file in a folder
-    
-    @param folder: The folder to search for the file
-    @param name: the name of the file
-    @return: the full name of the file if it exists,
-             None otherwise
-    """
-    for root, subFolders, files in os.walk(folder):
-        for filePath in files:
-            if (filePath == name):
-                return os.path.join(folder,filePath)
-    return None
-
-def iasFindFile(fileName,fileType=None):
-    """
-    Looks for a file named fileName in the hierarchy of IAS folders:
-    * current folder (only if it ends with src for example)
-    * module folders (lib for example)
-    * parent module folders (../lib for example)
-    * $IAS_ROOT folders
-    
-    The fileType tells where the file must be looked for and improves
-    the response time of the script.
-    
-    
-    @param fileName: The name of the file to look for
-    @param fileType: the type (iasFileType) of file to look for (example BINARY,LIB...
-    @return: if the file is found The full path name of the file
-             otherwise throws a IOError exception
-    """
-    iasRoot=os.environ['IAS_ROOT']
-    if fileType:
-        fileType = fileType.lower()
-        if not fileType in iasFileType:
-            raise ValueError("Unrecognized fileType '"+fileType+"' not in "+str(iasFileType))
-        folders = (fileType,)
-    else:
-        # No folder passed: search in all the folders
-        folders = iasFileType
-    
-    for folder in folders:
-        # Search in the current folder if its terminates with folder
-        # i.e. search in lib if the name of the current folder terminates with lib
-        currentFolder = os.getcwd()
-        if currentFolder.endswith(folder):
-            filePath = recursivelyLookForFile(currentFolder,fileName)
-            if filePath is not None:
-                return filePath
-                
-        # Assume to be in the root of a folder 
-        ## i.e. search in ./lib
-        folderToSearch=os.path.join(currentFolder,folder)
-        if os.path.exists(folderToSearch) and os.path.isdir(folderToSearch):
-            filePath = recursivelyLookForFile(folderToSearch,fileName)
-            if filePath is not None:
-                return filePath
-        # Assume to be in a  module folder and look for the file in the passed folder
-        # for example current folder is ../src, search in ../lib
-        folderToSearch=os.path.join(currentFolder,"..",folder)
-        if os.path.exists(folderToSearch) and os.path.isdir(folderToSearch):
-            filePath = recursivelyLookForFile(folderToSearch,fileName)
-            if filePath is not None:
-                return filePath
-        # Finally search in IAS_ROOT
-        folderToSearch=os.path.join(iasRoot,folder)
-        if os.path.exists(folderToSearch) and os.path.isdir(folderToSearch):
-            filePath = recursivelyLookForFile(folderToSearch,fileName)
-            if filePath is not None:
-                return filePath
-    # Bad luck!
-    raise IOError(fileName+" not found")
+from IASTools.FileSupport import FileSupport
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Search for a file in the hierarchy of IAS folders.')
     parser.add_argument(
         '-t',
         '--type',
-        help='The type of files to search for: '+str(iasFileType),
+        help='The type of files to search for: '+str(FileSupport.iasFileType),
         dest="fileType",
         action='store',
         default=None)
@@ -103,9 +23,11 @@ if __name__ == '__main__':
     
     try:
         if not args.fileType is None:
-            filePath=iasFindFile(args.fileName, args.fileType)
+            fileSupport = FileSupport(args.fileName, args.fileType)
+            filePath=fileSupport.findFile()
         else:
-            filePath=iasFindFile(args.fileName)
+            fileSupport = FileSupport(args.fileName)
+            filePath=fileSupport.findFile()
         print filePath
     except IOError as e:
         print "File not found"
