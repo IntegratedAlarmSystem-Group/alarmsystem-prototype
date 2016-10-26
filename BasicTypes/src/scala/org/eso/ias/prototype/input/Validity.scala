d92417c88cf85864bdf017b649e43c85052b8dd3
Alessandro Caproni <acaproni@eso.org>
2016-10-26 15:46:28 +0200

    Adds methods to compare Validities

--- BasicTypes/src/scala/org/eso/ias/prototype/input/Validity.scala
+++ BasicTypes/src/scala/org/eso/ias/prototype/input/Validity.scala
@@ -7,8 +7,32 @@ package org.eso.ias.prototype.input
  * @author acaproni
  */
 object Validity extends Enumeration {
-  val Reliable = Value("Reliable") // Reliable
-  val Unreliable = Value("Unreliable") // Unreliable
+  
+  // Scala uses the ID of the Value to order 
+  // the Validity following the declaration order
+  // of the Values. To avoid confusion we set the
+  // id esplicitly
+  val Unreliable = Value(0,"Unreliable") // Unreliable
+  val Reliable = Value(1,"Reliable") // Reliable
+  
+  /**
+   * Scans all the values of this object to get the min id.
+   * 
+   * Note that scala Enumeration returns the maxId but for our
+   * purposes it is better to reason in terms of min values.
+   * However the cost of this method is negligible because 
+   * it is executed only once for very few elements.
+   * 
+   * We do not want to hardcode the value because it easy
+   * to forget updating the hardcoded value when adding new
+   * Valitiy.Values to this Enumeration
+   */
+  private val minID: Int = {
+    val validities =Validity.values.toList
+    val ids = for (validity <- validities) yield validity.id 
+    val sorted = ids.sortWith( (l,r) => l<=r)
+    sorted(0)
+  }
   
   /**
    * Check if the passed validity is "valid"
@@ -18,4 +42,22 @@ object Validity extends Enumeration {
    * 			   False otherwise
    */
   def isValid(v: Validity.Value): Boolean = v==Reliable
+  
+  def min(left: Validity.Value, right: Validity.Value): Validity.Value = {
+    Validity.ValueOrdering.min(left, right)
+  }
+  
+  /**
+   * Look for the min value between all the validities in the
+   * passed list.
+   * 
+   * @param vals The list of Validity to check for the min value
+   * @return The min validity between those in the passed 
+   *         non-empty List;
+   *         Reliable if the list is empty
+   */
+  def min(vals: List[Validity.Value]): Validity.Value = {
+        if (vals.isEmpty) Reliable
+        else vals.sortWith( (l,r) => Validity.ValueOrdering.lteq(l, r))(0)
+  }
 }
--- BasicTypes/test/runTests.sh
+++ BasicTypes/test/runTests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestAlarmValue
 iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestLongMP
+iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestValidity
