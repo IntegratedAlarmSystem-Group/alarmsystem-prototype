1ae51e3113d7078a80e5a42d5f72fc4b2e33313d
Alessandro Caproni <acaproni@eso.org>
2016-10-28 15:45:01 +0200

    Renames TestLongMP to TestTypedMonitorPoint

--- BasicTypes/test/runTests.sh
+++ BasicTypes/test/runTests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestAlarmValue
-iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestLongMP
+iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestTypedMonitorPoint
 iasRun.py -l s org.scalatest.run org.eso.ias.basictypes.test.TestValidity
