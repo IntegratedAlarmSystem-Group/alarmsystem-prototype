1d4c82d545065686dd3d88be2356e40d30c3ea98
Alessandro Caproni <acaproni@eso.org>
2016-10-28 15:41:56 +0200

    MonitorPoint factory methods moved into TypedMonitorPoint
    Factory methods have been moved out of the MonitorPoint into the new
    TypedMonitorPoint. Type checking on the implemented types is done at
    build time in case we wish to keep types under control and provide new
    specialized types.
    
    MonitorPoint[A] is now merely a value holder for values of type A
    extending MonitorPointBase so that we do not have to bring the type
    around while dealing with different types of monitor points.
    
    Terst updated accordingly

--- BasicTypes/src/scala/org/eso/ias/prototype/input/MonitorPoint.scala
+++ BasicTypes/src/scala/org/eso/ias/prototype/input/MonitorPoint.scala
@@ -31,6 +31,7 @@ class MonitorPointValue[A](
  * <code>MonitorPoint</code> is immutable.
  * 
  * @param ident The unique ID of the monitor point
+ * @param actualValue The value of the monitor point
  * @param mode The operational mode
  * @param valid: The validity of the monitor point
  * @author acaproni
@@ -40,44 +41,4 @@ class MonitorPoint[A] protected (
     val actualValue: Option[MonitorPointValue[A]] = None, // Uninitialized at build time
     mode: OperationalMode.Mode = OperationalMode.Unknown,
     valid: Validity.Value = Validity.Unreliable) 
-extends MonitorPointBase(ident,mode,valid) {
-
-  /**
-   * Update the value of the monitor point
-   * 
-   * @param v: the new value of the monitor point
-   * @return a new MonitorPoint with the value set to v
-   * 
-   */
-  def updateValue(v: A): MonitorPoint[A] = {
-    val opt: Option[MonitorPointValue[A]] =  if (v==None) None else Some(new MonitorPointValue[A](v))
-    new MonitorPoint[A](id,opt,runningMode,validity)
-  }
-  
-  /**
-   * Update the operational mode of the monitor point
-   * 
-   * @param mode The new operational mode
-   * @return A new monitor point with the update operational mode 
-   */
-  def switchOperationalMode(mode: OperationalMode.Mode): MonitorPoint[A] = {
-    new MonitorPoint[A](id,actualValue,mode,validity)
-  }
-  
-  /**
-   * Update the validity of the monitor point
-   * 
-   * @param validity The new validity
-   * @return A new monitor point with the passed validity
-   */
-  def setValidity(newValidity: Validity.Value): MonitorPoint[A] = {
-    new MonitorPoint[A](id,actualValue,runningMode,newValidity)
-  }
-  
-  override def toString(): String = {
-    "Monitor point " + id.toString() + "\n\t" + 
-    runningMode.toString() + "\n\t" +
-    validity.toString() +"\n\t" +
-    (if (actualValue==None) "No value" else actualValue.get.toString())
-  }
-}
+extends MonitorPointBase(ident,mode,valid) 
--- BasicTypes/test/scala/org/eso/ias/basictypes/test/TestLongMP.scala
+++ BasicTypes/test/scala/org/eso/ias/basictypes/test/TestLongMP.scala
@@ -1,10 +1,11 @@
 package org.eso.ias.basictypes.test
 
 import org.scalatest.FlatSpec
-import org.eso.ias.prototype.input.typedmp.LongMP
 import org.eso.ias.prototype.input.Validity
 import org.eso.ias.prototype.input.OperationalMode
 import org.eso.ias.prototype.input.Identifier
+import org.eso.ias.prototype.input.typedmp.TypedMonitorPoint
+import org.eso.ias.prototype.input.MonitorPointValue
 
 /**
  * Test the LongMP
@@ -13,11 +14,11 @@ import org.eso.ias.prototype.input.Identifier
  */
 class TestLongMP extends FlatSpec {
   // The ID of the alarms built bin this test 
-  val id = new Identifier("LongMPID", "ParentID"+Identifier.separator+"host")
+  val id = new Identifier("TypedLongMPID", "ParentID"+Identifier.separator+"host")
   
-  "A long monitor point" must "have an ID" in {
-    val mp: LongMP = new LongMP(id)
-    assert(mp.mpID == id)
+  "A typed monitor point" must "have an ID" in {
+    val mp: TypedMonitorPoint[Long] = TypedMonitorPoint.typedMonitor(id)
+    assert(mp.id == id)
     
     assert (!mp.actualValue.isDefined)
     assert(mp.runningMode == OperationalMode.Unknown)
@@ -25,10 +26,10 @@ class TestLongMP extends FlatSpec {
   }
   
   it must "Have the same ID after changing other props" in {
-    val mp: LongMP = new LongMP(id)
+    val mp: TypedMonitorPoint[Long] = TypedMonitorPoint.typedMonitor(id)
     
     // Change the value of the previous MP
-    val mp2 = mp.updateValue(3L)
+    val mp2 = TypedMonitorPoint.updateValue(mp, 3L)
     assert(mp2.id == mp.id)
     assert(mp2.actualValue.isDefined)
     assert(mp2.actualValue.get.value == 3L)
@@ -38,7 +39,7 @@ class TestLongMP extends FlatSpec {
     assert(mp2.validity == Validity.Unreliable)
     
     // Change validity of the previous MP
-    val mp3 = mp2.setValidity(Validity.Reliable);
+    val mp3 = TypedMonitorPoint.updateValidity(mp2,Validity.Reliable)
     assert(mp3.id == mp.id)
     assert(mp3.actualValue.isDefined)
     assert(mp3.actualValue.get  == mp2.actualValue.get)
@@ -46,7 +47,7 @@ class TestLongMP extends FlatSpec {
     assert(mp3.validity == Validity.Reliable)
     
     // Change mode of the previous MP
-    val mp4 = mp3.switchOperationalMode(OperationalMode.Operational)
+    val mp4 = TypedMonitorPoint.updateMode(mp3,OperationalMode.Operational)
     assert(mp4.id == mp.id)
     assert(mp4.actualValue.isDefined)
     assert(mp4.actualValue.get  == mp3.actualValue.get)
