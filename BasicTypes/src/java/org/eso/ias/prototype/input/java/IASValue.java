a9b9719af7c2d285c1e3a727c43826ee549362c6
Alessandro Caproni <acaproni@eso.org>
2016-11-22 10:19:33 +0100

    Various improvements
    The scope of the read-only properties is public.
    Provide methods to update the value and the mode (note that being
    immutable they return a new object)

--- BasicTypes/src/java/org/eso/ias/prototype/input/java/IASValue.java
+++ BasicTypes/src/java/org/eso/ias/prototype/input/java/IASValue.java
@@ -2,9 +2,12 @@ package org.eso.ias.prototype.input.java;
 
 import org.eso.ias.prototype.input.Identifier;
 import org.eso.ias.prototype.input.java.OperationalMode;
-
+import java.lang.StringBuilder;
 /**
- * The heterogeneous inputs in the java code.
+ * The view of a heterogeneous inputs in the java code.
+ * 
+ * Objects of this class are immutable i.e. updating returns
+ * a new immutable object
  * 
  * @author acaproni
  *
@@ -12,28 +15,28 @@ import org.eso.ias.prototype.input.java.OperationalMode;
 public class IASValue<T> {
 	
 	/**
-	 * The value of the input
+	 * The value of the HIO
 	 */
-	final T value;
+	public final T value;
 	
 	/**
-	 * The time when the value has been assigned to the input
+	 * The time when the value has been assigned to the HIO
 	 */
-	final long timestamp;
+	public final long timestamp;
 	
 	/**
 	 * The mode of the input
 	 * 
 	 * @see OperationalMode
 	 */
-	final OperationalMode mode;
+	public final OperationalMode mode;
 	
 	/**
 	 * The identifier of the input
 	 * 
 	 * @see Identifier
 	 */
-	final String id;
+	public final String id;
 	
 	/**
 	 * The identifier of the input concatenated with
@@ -41,14 +44,40 @@ public class IASValue<T> {
 	 * 
 	 * @see Identifier
 	 */
-	final String runningId;
+	public final String runningId;
 	
 	/**
 	 * The IAS representation of the type of this input.
 	 * 
 	 * @see IASTypes
 	 */
-	final IASTypes valueType;
+	public final IASTypes valueType;
+	
+	/**
+	 * Build a new IASValue with the passed mode
+	 * 
+	 * @param newMode The mode to set in the new IASValue
+	 * @return The new IASValue with the updated mode
+	 */
+	public IASValue<T> updateMode(OperationalMode newMode) {
+		if (newMode==null) {
+			throw new NullPointerException("The mode can't be null");
+		}
+		return new IASValue<T>(value,System.currentTimeMillis(),newMode,id,runningId,valueType);
+	}
+	
+	/**
+	 * Build a new IASValue with the passed value
+	 * 
+	 * @param newValue The value to set in the new IASValue
+	 * @return The new IASValue with the updated value
+	 */
+	public IASValue<T> updateValue(T newValue) {
+		if (newValue==null) {
+			throw new NullPointerException("The value can't be null");
+		}
+		return new IASValue<T>(newValue,System.currentTimeMillis(),mode,id,runningId,valueType);
+	}
 	
 
 	/**
@@ -76,5 +105,23 @@ public class IASValue<T> {
 		this.id=id;
 		this.runningId=runningId;
 		this.valueType=valueType;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder ret = new StringBuilder("IASValue(id=");
+		ret.append(id);
+		ret.append(", runningID=");
+		ret.append(runningId);
+		ret.append(", value=");
+		ret.append(value);
+		ret.append(", timestamp=");
+		ret.append(timestamp);
+		ret.append(", mode=");
+		ret.append(mode);
+		ret.append(", type=");
+		ret.append(valueType);
+		ret.append(")");
+		return ret.toString();
 	}
 }
