a146e3bc9192205ac5d0fb9df71c2a734f651fdd
Alessandro Caproni <acaproni@eso.org>
2016-12-06 17:53:53 +0100

    The threshold for the min/Max (test still failing...)

--- CompElement/test/runTests.sh
+++ CompElement/test/runTests.sh
@@ -2,4 +2,5 @@
 iasRun.py -l s org.scalatest.run org.eso.ias.component.test.TestTransferFunctionSetting
 iasRun.py -l s org.scalatest.run org.eso.ias.component.test.TestComponent
 iasRun.py -l s org.scalatest.run org.eso.ias.component.test.TestTransferFunction
+iasRun.py -l s org.scalatest.run org.eso.ias.component.test.TestMinMaxThreshold
 
