423b9aaf431e138fd99c3e8285548fd9866fa1b2
Alessandro Caproni <acaproni@eso.org>
2016-12-09 10:48:58 +0100

    More informantion on the expected and found type of the hio

--- CompElement/src/scala/org/eso/ias/prototype/compele/exceptions/TypeMismatchException.scala
+++ CompElement/src/scala/org/eso/ias/prototype/compele/exceptions/TypeMismatchException.scala
@@ -1,9 +1,15 @@
 package org.eso.ias.prototype.compele.exceptions
 
+import org.eso.ias.prototype.input.java.IASTypes
+
 /**
  * Exception thrown by the TF executor when the type
  * of a HIOs does not match with the expected on
+ * 
+ * @param hioId: the ID of the mismatched HIO
+ * @param actualType: the type of the HIO
+ * @param expectedType: the expected type of the HIO
  */
-class TypeMismatchException(hioId: String) 
-extends Exception("Type mismatch for HIO "+hioId) {
+class TypeMismatchException(hioId: String, actualType: IASTypes, expectedType: IASTypes) 
+extends Exception("Type mismatch for HIO "+hioId+": expected was "+expectedType+" but "+actualType+" found") {
 }
